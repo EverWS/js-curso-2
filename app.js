1c2cf608ad0f1614a98e66f26eadcc54fdd45d3c
Everton Silva <everws.83@gmail.com>
2024-06-24 13:46:14 -0300

    challenge finished

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ function verificarConsole() {
     console.log('O botão foi clicado!');
 }
 
+function verificarAlerta() {
+    alert('Eu amo JS!');
+}
+
+function verificaCidade() {
+    let cidade = prompt('Qual o nome de uma cidade do Brasil?');
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+
+function resultadoSoma() {
+    let num1 = Number(prompt('Digite um número inteiro:'));
+    let num2 = Number(prompt('Digite outro número inteiro:'));
+    let resultado = num1 + num2;
+    alert(`A soma dos dois números inteiros é ${resultado}`);
+}
